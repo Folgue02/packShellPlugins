c764eda13e19091635f230a14e192a7469a3f851
Folgue02 <davidfolru@gmail.com>
2020-05-14 13:07:24 +0200

    More changes to the tree.py plugin

--- tree.py
+++ tree.py
@@ -3,8 +3,6 @@ from sys import argv as pars
 del pars[0]
 
 
-
-
 path = os.getcwd()
 
 if len(pars) > 0:
@@ -14,7 +12,8 @@ if len(pars) > 0:
 gap = 2
 
 
-
+if not os.path.isdir(path):
+	print(f"Couldn't find the specified path: '{path}'")
 
 def buildString(times, char=" "):
 	result = ""
@@ -28,12 +27,14 @@ def branch(lvl, path):
 	nodes = os.listdir(path)
 
 	for node in nodes:
-		print(buildString(lvl*gap) + node)
+		
 
 		if os.path.isdir(os.path.join(path, node)):
 			branch(lvl+1, os.path.join(path, node))
 
 		else:
-			continue
-
-branch(0, os.getcwd())
+			print(buildString(lvl*gap) + node)
+try:
+	branch(0, path)
+except PermissionError:
+	print("Permission denied.")
